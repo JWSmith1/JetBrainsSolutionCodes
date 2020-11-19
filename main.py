bfa63fb1f8e2eb71e06d04b6ddda08c939abf9ba
Jacob Smith <74027890+JWSmith1@users.noreply.github.com>
2020-11-19 14:05:00 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+class Angel:
+    color = "white"
+    feature = "wings"
+    home = "Heaven"
+
+
+class Demon:
+    color = "red"
+    feature = "horns"
+    home = "Hell"
+
+#Angel instance
+my_angel = Angel()
+print(my_angel.color)
+print(my_angel.feature)
+print(my_angel.home)
+
+#Demon instance
+my_demon = Demon()
+print(my_demon.color)
+print(my_demon.feature)
+print(my_demon.home)
